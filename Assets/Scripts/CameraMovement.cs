2023fff12ed09c3adfa4a093b17b54815cd8f3e8
louisme.2014@gmail.com <louisme.2014@gmail.com>
2023-08-22 18:59:21 +0200

    first commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 3f;
+
+    void Update()
+    {
+        // Handle Keyboard Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.Translate(moveDirection * movementSpeed * Time.deltaTime);
+
+        // Handle Mouse Rotation
+        float mouseX = Input.GetAxis("Mouse X");
+        Vector3 rotationAmount = new Vector3(0f, mouseX * rotationSpeed, 0f);
+        transform.Rotate(rotationAmount);
+    }
+}
+
